bee4a55796bdc2296e4a7a9804cbd5c9e6bec555
Ken Browning <ken.browning@hautelook.com>
2016-06-09 18:28:05 -0700

    Use Headers::forEach as a fallback when iterator access fails

--- index.js
+++ index.js
@@ -18,6 +18,26 @@ function isJsonContentType(contentType) {
 }
 
 function parseHeaders(headers) {
+  try {
+    return parseHeadersViaIterator(headers);
+  } catch (err) {
+    // node-fetch does not have an iterator
+    return parseHeadersViaForEach(headers);
+  }
+}
+
+function parseHeadersViaIterator(headers) {
+  var result = {};
+  for (var headerTuple of headers) {
+    var key = headerTuple[0];
+    var value = headerTuple[1];
+    result[key] = result[key] || [];
+    result[key].push(value);
+  }
+  return result;
+}
+
+function parseHeadersViaForEach(headers) {
   var result = {};
   headers.forEach(function(value, key) {
     result[key] = result[key] || [];
